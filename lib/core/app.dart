6db6e0945d3ddc3a47cfe2e75923a5d0d1e6f7f7
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-16 19:52:16 +0530

    app.dart : home page add

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:,
+      home: HomePage(),
     );
   }
 }
