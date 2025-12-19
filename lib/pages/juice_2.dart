9c9f068b68cec9305b8ce4d03f968611da4aa81c
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2025-12-19 22:00:56 +0530

    juice_2.dart : second page add

---
+++ lib/pages/juice_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Juice2 extends StatefulWidget {
+  const Juice2({super.key});
+
+  @override
+  State<Juice2> createState() => _Juice2State();
+}
+
+class _Juice2State extends State<Juice2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
